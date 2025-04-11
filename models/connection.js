4989ef4a9cbf0045feccc42ffa026fedb48dc839
Margaux <margaux.courageux@gmail.com>
2025-04-11 21:08:23 +0200

    ajout du fichier env et variable env

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://margauxcourageux:mUqMEXpXz6vohqui@clustermc.eeyow1k.mongodb.net/weatherapp';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
